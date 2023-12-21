05243cd59c69982a191acdd692f4b69c5947922c
benjmnxu <123025188+benjmnxu@users.noreply.github.com>
2023-12-20 21:41:30 -0500

    please

--- app.py
+++ app.py
@@ -92,25 +92,29 @@ col1, col2 = st.columns(2)
 df = load_data()
 
 col1.write("**Languages Used Over Time**")
-languages = col1.multiselect("Primary language", options = df["primary_language"].unique(), default = ["JavaScript", "Python", "Java", "C++", "PHP"])
+col1.image("languages.png")
+# languages = col1.multiselect("Primary language", options = df["primary_language"].unique(), default = ["JavaScript", "Python", "Java", "C++", "PHP"])
 
-languageDF = load_language_chart(languages)
-if languageDF is not None:
-    col1.area_chart(languageDF, x="year", y=languages)
+# languageDF = load_language_chart(languages)
+# if languageDF is not None:
+#     col1.area_chart(languageDF, x="year", y=languages)
 
 
 col2.write("**Licences Being Used Over Time**")
-licences = col2.multiselect("Type of Licence", options = df["licence"].unique(), default = ["MIT License", "None", "Apache License 2.0", "GNU General Public License v3.0"])
+col2.image("languages.png")
+# licences = col2.multiselect("Type of Licence", options = df["licence"].unique(), default = ["MIT License", "None", "Apache License 2.0", "GNU General Public License v3.0"])
 
-licenceDF = load_licence_chart(licences)
-if licenceDF is not None:
-    col2.area_chart(licenceDF, x="year", y=licences)
+# licenceDF = load_licence_chart(licences)
+# if licenceDF is not None:
+#     col2.area_chart(licenceDF, x="year", y=licences)
 
 st.write("**Most Frequent Pairings of Major Languages**")
 
 number = 5 # int(st.number_input("Insert a number", format = "%d", min_value = 2, value=5, placeholder="Type a number..."))
 st.write('Showing the ', number, ' most common languages' )
-st.image("heatmap.png")
+heatmap = "heatmap.png"
+st.image(heatmap, output_format = "png")
+st.markdown(f"<img style='max-width: 100%;max-height: 100%;' src='data:image/png;base64, heatmap.png'/>", unsafe_allow_html=True)
 # languages, z = load_commonly_combined(number)
 # if languages is not None and z is not None:
 #     fig = px.imshow(z, labels = dict(x = "Language", y = "Language", color = "Occurrences"), x=languages, y = languages, text_auto=True)
